184a83938d3a93fc40d024073918dc4595175bb8
Kawsar Ahmed <coder.kawsar@gmail.com>
2022-10-17 23:48:57 +0600

    CRUD using rtk query done

--- src/components/video/related/RelatedVideos.js
+++ src/components/video/related/RelatedVideos.js
@@ -1,9 +1,41 @@
+import { useGetRelatedVideosQuery } from "../../../features/api/apiSlice";
+import Error from "../../ui/Error";
+import RelatedVideoLoader from "../../ui/loaders/RelatedVideoLoader";
 import RelatedVideo from "./RelatedVideo";
 
-export default function RelatedVideos() {
-    return (
-        <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
-            <RelatedVideo />
-        </div>
+export default function RelatedVideos({ id, title }) {
+  const {
+    data: videos,
+    isLoading,
+    isError,
+  } = useGetRelatedVideosQuery({ id, title });
+
+  let content;
+  if (isLoading) {
+    content = (
+      <>
+        <RelatedVideoLoader />
+        <RelatedVideoLoader />
+        <RelatedVideoLoader />
+      </>
     );
+  }
+  if (isError) {
+    content = <Error message="There was an error" />;
+  }
+  if (!isLoading && !isError && videos.length === 1) {
+    content = <Error message="No related videos here!"></Error>;
+  }
+  if (!isLoading && !isError && videos.length > 1) {
+    const relatedVideosToShow = videos.filter((video) => video?.id !== id);
+    content = relatedVideosToShow.map((video) => (
+      <RelatedVideo key={video?.id} video={video} />
+    ));
+  }
+
+  return (
+    <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
+      {content}
+    </div>
+  );
 }
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,24 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
+
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  let content = null;
+  if (isLoading) {
+    content = <VideoLoader />;
+  }
+  if (isError) {
+    content = <Error />;
+  }
+  if (!isLoading && !isError && videos.length === 0) {
+    content = <p>No videos found!</p>;
+  }
+  if (!isLoading && !isError && videos.length > 0) {
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  }
+  return <>{content}</>;
 }
